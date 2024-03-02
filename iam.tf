beb4dc57eedda29570b179f3d5695dd4ef17adf3
Oleksandr Kuzminskyi <aleks@infrahouse.com>
2024-03-02 11:37:50 -0800

    Use instance-profile from GitHub

--- iam.tf
+++ iam.tf
@@ -1,6 +1,5 @@
 module "webserver_profile" {
-  source       = "infrahouse/instance-profile/aws"
-  version      = "~> 1.0"
+  source       = "git::https://github.com/infrahouse/terraform-aws-instance-profile.git?ref=1.3.0"
   profile_name = var.instance_profile
   permissions  = var.webserver_permissions
 }
