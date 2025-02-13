c2fd0d4704b6dd25c94a029e56afac6a730a8b1c
xOtanix <maksym@tinyfish.io>
2025-02-13 19:20:02 +0200

    Bump version: 4.9.0 → 4.10.0

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 locals {
   module         = "infrahouse/website-pod/aws"
-  module_version = "4.9.0"
+  module_version = "4.10.0"
   default_module_tags = merge(
     {
       environment : var.environment
