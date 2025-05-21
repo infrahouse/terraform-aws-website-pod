4dfb08491e6c2c0a6a5f26b3b4405f63f4b242bc
Oleksandr Kuzminskyi <1763754+akuzminsky@users.noreply.github.com>
2025-05-20 18:35:41 -0700

    Enable SSM on website-pod instances (#71)

--- iam.tf
+++ iam.tf
@@ -6,7 +6,7 @@ resource "random_string" "profile_suffix" {
 
 module "instance_profile" {
   source          = "registry.infrahouse.com/infrahouse/instance-profile/aws"
-  version         = "1.6.1"
+  version         = "1.8.1"
   profile_name    = "${var.service_name}-instance-${random_string.profile_suffix.result}"
   role_name       = var.instance_role_name
   permissions     = var.instance_profile_permissions == null ? data.aws_iam_policy_document.default_permissions.json : var.instance_profile_permissions
