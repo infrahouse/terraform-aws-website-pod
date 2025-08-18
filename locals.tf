fee794411dc2742f9d19d15c79173d58d4dd8025
Oleksandr Kuzminskyi <aleks@infrahouse.com>
2025-08-18 12:53:29 -0700

    Bump version: 5.5.0 → 5.6.0

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 locals {
   module         = "infrahouse/website-pod/aws"
-  module_version = "5.5.0"
+  module_version = "5.6.0"
   default_module_tags = merge(
     {
       environment : var.environment
