49319c078e4abfbb61fad1cba431a9d7df175e9a
Maksym Kryva <maksym@tinyfish.io>
2025-08-31 10:45:55 +0300

    feat: add caa records for created dns records (#87)
    * feat: add caa records for created dns records
    
    * fix dns
    
    * add tests

--- dns.tf
+++ dns.tf
@@ -10,3 +10,18 @@ resource "aws_route53_record" "extra" {
     evaluate_target_health = true
   }
 }
+
+resource "aws_route53_record" "extra_caa_amazon" {
+  provider = aws.dns
+  count    = var.assume_dns ? length(var.dns_a_records) : 0
+  zone_id  = var.zone_id
+  name     = trimprefix(join(".", [var.dns_a_records[count.index], data.aws_route53_zone.webserver_zone.name]), ".")
+  type     = "CAA"
+  ttl      = 300
+  records = [
+    "0 issue \"amazon.com\"",
+    "0 issue \"amazontrust.com\"",
+    "0 issue \"awstrust.com\"",
+    "0 issue \"amazonaws.com\""
+  ]
+}
