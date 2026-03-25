43900e92375d6ca563399a1f8af2a1974e1aa76d
Oleksandr Kuzminskyi <aleks@infrahouse.com>
2026-03-25 10:10:54 -0700

    fix: add abort_incomplete_multipart_upload to Athena results bucket lifecycle

--- glue.tf
+++ glue.tf
@@ -27,6 +27,10 @@ resource "aws_s3_bucket_lifecycle_configuration" "athena_results" {
     expiration {
       days = 30
     }
+
+    abort_incomplete_multipart_upload {
+      days_after_initiation = 7
+    }
   }
 }
 
